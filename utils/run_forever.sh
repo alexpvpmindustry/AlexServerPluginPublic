43bd0acd856d6dfa5321bdafccf77360158e709f
alexpvpmindustry <alex.mindustry@gmail.com>
2021-07-05 12:22:10 +0800

    update

---
+++ utils/run_forever.sh
@@ -0,0 +1,6 @@
+while :
+do
+  echo "starting game server command"
+  java -jar server-release.jar
+  sleep 5
+done
--- utils/screen_server_start.sh
+++ utils/screen_server_start.sh
@@ -1,7 +1,7 @@
 SCRNAME="${PWD##*/}"
 echo creating screen $SCRNAME
 if ! screen -ls | grep -q $SCRNAME; then
-  screen -S $SCRNAME -dm bash -c 'java -jar server-release.jar'
+  screen -S $SCRNAME -dm bash -c './run_forever.sh'
   echo screen $SCRNAME created, server started.
 else
   echo the screen $SCRNAME exists, please connect to it instead.
